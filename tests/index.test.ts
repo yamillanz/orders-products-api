c2f1432f50450c388a71ccd9dc98e39958c301aa
Yamil Lanz <yamil.lanz@naranja.com>
2022-06-21 11:34:11 -0400

    bubble algorithm

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,5 +1,7 @@
-describe('Principal', () => {
-  it('first test', () => {
-    expect(true).toBe(true);
+import bubbleSort from '../src/index';
+
+describe('Array sort...', () => {
+  it('bubbleSort algorithm', () => {
+    expect(bubbleSort([1, 3, 2, 5, 4])).toEqual([1, 2, 3, 4, 5]);
   });
 });
